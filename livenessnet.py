b363d42afb529752f435bad5dc728870c8576f5e
Ashish Kumar <ashish.8882828@gmail.com>
2019-05-18 19:20:36 -0400

    added a new class for CNN3

--- livenessnet.py
+++ livenessnet.py
@@ -63,7 +63,7 @@ class LivenessNet:
 		# return the constructed network architecture
 		return model
 
-class LivenessNetCNN:
+class LivenessNetCNN1:
 	@staticmethod
 	def build(width, height, depth, classes):
 		# initialize the model along with the input shape to be
@@ -90,6 +90,57 @@ class LivenessNetCNN:
 
 		# second CONV => RELU => CONV => RELU => POOL layer set
 		model.add(Conv2D(32, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Conv2D(32, (3, 3), padding="same"))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# first (and only) set of FC => RELU layers
+		model.add(Flatten())
+		model.add(Dense(64))
+		model.add(Activation("relu"))
+		model.add(Dropout(0.5))
+
+		# softmax classifier
+		model.add(Dense(classes))
+		model.add(Activation("softmax"))
+
+		# saving model structure as png
+		plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+		# return the constructed network architecture
+		return model
+
+
+class LivenessNetCNN2:
+	@staticmethod
+	def build(width, height, depth, classes):
+		# initialize the model along with the input shape to be
+		# "channels last" and the channels dimension itself
+		model = Sequential()
+		inputShape = (height, width, depth)
+		chanDim = -1
+
+		# if we are using "channels first", update the input shape
+		# and channels dimension
+		if K.image_data_format() == "channels_first":
+			inputShape = (depth, height, width)
+			chanDim = 1
+
+		# first CONV => RELU => CONV => RELU => POOL layer set
+		model.add(Conv2D(16, (5, 5), padding="same",
+			input_shape=inputShape))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Conv2D(16, (5, 5), padding="same"))
+		model.add(Activation("relu"))
+		model.add(MaxPooling2D(pool_size=(2, 2)))
+		model.add(Dropout(0.25))
+
+		# second CONV => RELU => CONV => RELU => POOL layer set
+		model.add(Conv2D(32, (5, 5), padding="same"))
 		model.add(Activation("relu"))
 		model.add(MaxPooling2D(pool_size=(2, 2)))
 		model.add(Conv2D(32, (3, 3), padding="same"))
